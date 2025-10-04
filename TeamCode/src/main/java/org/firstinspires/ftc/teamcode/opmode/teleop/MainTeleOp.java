c2fd714049ec20d875ef98e789afab0c86c90ec3
mok <msm.kasid@gmail.com>
2025-10-04 13:36:37 -0600

    #0 in readme commit summaries.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/MainTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/MainTeleOp.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.opmode.teleop;
 
 import static com.qualcomm.robotcore.hardware.DcMotor.ZeroPowerBehavior.BRAKE;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
@@ -14,36 +15,32 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
-/*
- * This file includes a teleop (driver-controlled) file for the goBILDA® StarterBot for the
- * 2025-2026 FIRST® Tech Challenge season DECODE™. It leverages a differential/Skid-Steer
- * system for robot mobility, one high-speed motor driving two "launcher wheels", and two servos
- * which feed that launcher.
- *
- * Likely the most niche concept we'll use in this example is closed-loop motor velocity control.
- * This control method reads the current speed as reported by the motor's encoder and applies a varying
- * amount of power to reach, and then hold a target velocity. The FTC SDK calls this control method
- * "RUN_USING_ENCODER". This contrasts to the default "RUN_WITHOUT_ENCODER" where you control the power
- * applied to the motor directly.
- * Since the dynamics of a launcher wheel system varies greatly from those of most other FTC mechanisms,
- * we will also need to adjust the "PIDF" coefficients with some that are a better fit for our application.
- */
+// PLEASE PLEASE READ THE README FILE BEFORE TOUCHING THE CODE
 
-@TeleOp(name = "MainTeleOp", group = "!")
+@TeleOp(name = "MainTeleOp2", group = "!")
 public class MainTeleOp extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
-        // Declare our motors
-        // Make sure your ID's match your configuration
+
+        // Declare our motors and empty variables for they're power
+
+        // drivebase
         DcMotor frontLeftMotor = hardwareMap.dcMotor.get("leftFront");
         DcMotor backLeftMotor = hardwareMap.dcMotor.get("leftBack");
         DcMotor frontRightMotor = hardwareMap.dcMotor.get("rightFront");
         DcMotor backRightMotor = hardwareMap.dcMotor.get("rightBack");
 
-        // Reverse the right side motors. This may be wrong for your setup.
-        // If your robot moves backwards when commanded to go forwards,
-        // reverse the left side instead.
-        // See the note about this earlier on this page.
+        double frontLeftPower = 0;
+        double frontRightPower = 0;
+        double backLeftPower = 0;
+        double backRightPower = 0;
+
+        // turret
+        DcMotor turretAccel = hardwareMap.dcMotor.get("turretAccelerator");
+
+        double turretAccelPower = 0;
+
+        // make all motors work in the same direction; counteract reverse motors
         frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
 
@@ -51,29 +48,47 @@ public class MainTeleOp extends LinearOpMode {
         backLeftMotor.setZeroPowerBehavior(BRAKE);
         frontRightMotor.setZeroPowerBehavior(BRAKE);
         backRightMotor.setZeroPowerBehavior(BRAKE);
+        turretAccel.setZeroPowerBehavior(BRAKE);
 
         waitForStart();
 
         if (isStopRequested()) return;
 
         while (opModeIsActive()) {
-            double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
-            double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
-            double rx = gamepad1.right_stick_x;
 
-            // Denominator is the largest motor power (absolute value) or 1
-            // This ensures all the powers maintain the same ratio,
-            // but only if at least one is out of the range [-1, 1]
+            // mecanum drivebase logic
+
+            double y = gamepad1.left_stick_y; // left stick y axis
+            double x = -gamepad1.left_stick_x * 1.1; // left stick x axis, multi to counteract imperfect strafing
+            double rx = gamepad1.right_stick_x; // right stick x axis
+
+            // CHECK GM 0 FOR MATH, search mecanum teleop
+            // IMPORTANT: REMOVE THE /2 BASED ON DRIVER PREFERENCE.
             double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
-            double frontLeftPower = ((y + x + rx) / denominator) /2;
-            double backLeftPower = ((y - x + rx) / denominator) /2;
-            double frontRightPower = ((y - x - rx) / denominator) /2;
-            double backRightPower = ((y + x - rx) / denominator) /2;
+            frontLeftPower = ((y - x + rx) / denominator) /2;
+            backLeftPower = ((y + x + rx) / denominator) /2;
+            frontRightPower = ((y - x - rx) / denominator) /2;
+            backRightPower = ((y + x - rx) / denominator) /2;
 
-            frontLeftMotor.setPower(frontLeftPower);
+
+            // turret logic
+
+            turretAccelPower = -gamepad1.right_trigger;  // literally just motor power
+
+            // eventually add the gecko wheel logic here
+
+
+            frontLeftMotor.setPower(frontLeftPower);  // kinda straightforward here. just set motors to the previously calculated power, and send that to telemetry as well
             backLeftMotor.setPower(backLeftPower);
             frontRightMotor.setPower(frontRightPower);
             backRightMotor.setPower(backRightPower);
+            turretAccel.setPower(turretAccelPower);
+
+            telemetry.addData("FL power", frontLeftPower);
+            telemetry.addData("BL power", backLeftPower);
+            telemetry.addData("FR power", frontRightPower);
+            telemetry.addData("BR power", backRightPower);
+            telemetry.addData("Turret Power", turretAccelPower);
         }
     }
 }
